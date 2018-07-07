3d7ed86005c14e9885b34416801c3443ba4ab039
wangchifu <wangchifu@gmail.com>
2018-07-07 20:41:45 +0800

    fix bug

--- resources/views/fixes/index.blade.php
+++ resources/views/fixes/index.blade.php
@@ -13,6 +13,9 @@
         </ol>
     </nav>
     <a href="{{ route('fixes.create') }}" class="btn btn-success btn-sm"><i class="fas fa-plus"></i> 新增報修項目</a>
+    <a href="{{ route('fixes.search',1) }}" class="btn btn-outline-success btn-sm"><i class="fas fa-check-square"></i> 處理完畢列表</a>
+    <a href="{{ route('fixes.search',2) }}" class="btn btn-outline-warning btn-sm"><i class="fas fa-exclamation-triangle"></i> 處理中列表</a>
+    <a href="{{ route('fixes.search',3) }}" class="btn btn-outline-danger btn-sm"><i class="fas fa-phone-square"></i> 申報中列表</a>
     <table class="table table-striped">
         <thead class="thead-light">
         <tr>
@@ -35,7 +38,7 @@
                       '3'=>'<i class="fas fa-phone-square text-danger"></i>'
                   ];
                   ?>
-                  {!! $icon[$fix->situation] !!} <a href="{{ route('fixes.search',$fix->situation) }}">{{ $situation[$fix->situation] }}</a>
+                  {!! $icon[$fix->situation] !!} {{ $situation[$fix->situation] }}
               </td>
               <td>
                   {{ substr($fix->created_at,0,10) }}
