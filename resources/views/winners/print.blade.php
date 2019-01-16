2ceaeccdf886d482909cfa0d7263c1bf24e93213
wangchifu <wangchifu@gmail.com>
2019-01-16 09:16:56 +0800

    fix bug

--- resources/views/winners/print.blade.php
+++ resources/views/winners/print.blade.php
@@ -8,6 +8,8 @@
     $cht_year = $today[0]-1911;
     $cht_d = sprintf("%02s",$today[2]);
     $cht_day = "中華民國{$cht_year}年{$today[1]}月{$cht_d}日";
+    $user = \App\User::where('job_title','校長')->first();
+    $p = $user->name;
 
         $data = "";
         foreach($has_classes as $k1=>$v1){
@@ -22,7 +24,8 @@
                 <div style=\"font-family:標楷體;font-size:70px;margin-top:220px;margin-left:120px;margin-right:120px;line-height: 120px;position:relative;height:1230px;\">
                 <br>
                 " . $content ."
-
+                <div style=\"font-family:標楷體;font-size:50px;position:absolute;bottom:130px;right:310px;\">校長</div>
+                <div style=\"font-family:標楷體;font-size:90px;position:absolute;bottom:130px;right:0px;\">{$p}</div>
 		        <div style=\"font-family:標楷體;font-size:30px;position:absolute;bottom:0px;right:0px;\">{$cht_day}　　　　</div>
 		        </div>
 		        <p style=\"page-break-after:always\"></p>
